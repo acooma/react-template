27f957bd86d4ec13dcfefd08977a0ff5bbad21f2
acooma <acoozee@gmail.com>
2020-01-18 21:53:24 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import AboutPage from "./pages/AboutPage";
+import ArticlesPage from "./pages/ArticlesPage.js";
+import ArticlesList from "./pages/ArticlesList.js";
+import NavBar from "./components/Navbar.js";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div id="">
+          <Route path="/" component={Home} exact />
+          <Route path="/about" component={AboutPage} exact />
+          <Route path="/articles-list" component={ArticlesList} exact />
+          <Route path="/article/:name" component={ArticlesPage} exact />
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AboutPage = () => (
+  <React.Fragment>
+    <h1>about me</h1>
+  </React.Fragment>
+);
+
+export default AboutPage;
